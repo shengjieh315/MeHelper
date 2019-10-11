2efa7e757d0a317d4f1328ae74aa664fcd9500f7
andysheng <553502618@qq.com>
2019-10-11 17:41:35 +0800

    alpha

--- app/src/main/java/com/kt/mehelper/ui/MainActivity.kt
+++ app/src/main/java/com/kt/mehelper/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kt.mehelper
+package com.kt.mehelper.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kt.mehelper.R
 
 class MainActivity : AppCompatActivity() {
 
