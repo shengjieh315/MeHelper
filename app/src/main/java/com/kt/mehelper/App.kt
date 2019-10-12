4af16ca512e366dff825e3cdf766abc123d9b958
andysheng <553502618@qq.com>
2019-10-12 15:47:48 +0800

    skin&package

--- app/src/main/java/com/kt/mehelper/App.kt
+++ app/src/main/java/com/kt/mehelper/App.kt
@@ -13,14 +13,14 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        SkinCompatManager.withoutActivity(this)
-            .addInflater(SkinAppCompatViewInflater())           // 基础控件换肤初始化
-            .addInflater(SkinMaterialViewInflater())            // material design 控件换肤初始化[可选]
-            .addInflater(SkinConstraintViewInflater())          // ConstraintLayout 控件换肤初始化[可选]
-            .addInflater(SkinCardViewInflater())                // CardView v7 控件换肤初始化[可选]
-            .setSkinStatusBarColorEnable(false)                     // 关闭状态栏换肤，默认打开[可选]
-            .setSkinWindowBackgroundEnable(false)                   // 关闭windowBackground换肤，默认打开[可选]
-            .loadSkin()
+//        SkinCompatManager.withoutActivity(this)
+//            .addInflater(SkinAppCompatViewInflater())           // 基础控件换肤初始化
+//            .addInflater(SkinMaterialViewInflater())            // material design 控件换肤初始化[可选]
+//            .addInflater(SkinConstraintViewInflater())          // ConstraintLayout 控件换肤初始化[可选]
+//            .addInflater(SkinCardViewInflater())                // CardView v7 控件换肤初始化[可选]
+//            .setSkinStatusBarColorEnable(false)                     // 关闭状态栏换肤，默认打开[可选]
+//            .setSkinWindowBackgroundEnable(false)                   // 关闭windowBackground换肤，默认打开[可选]
+//            .loadSkin()
 
     }
 
